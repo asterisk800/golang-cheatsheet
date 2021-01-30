e2cb179397e6ad68c20271e3a24ebe76987c2d37
Gudina H <asterisk800@users.noreply.github.com>
2021-01-30 13:58:03 -0800

    more notes

--- group-variables/group-variables.go
+++ group-variables/group-variables.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-// Use group declaration for a better readability
+// Use group declaration for a better readability and to group closely related items
 var (
 	name  string
 	speed float64
