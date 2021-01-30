32ea74d0e3bfb2c060d10f1577f659a82f0540b5
Gudina H <asterisk800@users.noreply.github.com>
2021-01-30 13:48:52 -0800

    Added note

--- redeclaration/redeclaration.go
+++ redeclaration/redeclaration.go
@@ -3,7 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	var safe bool // safe is existing variable, it will be assigned true, not redeclare
+	/*
+		:= can be used for redeclaraton and assignment in the same statement.
+		safe is and existing variable, it will be assigned true, not redeclare
+		but speed is declared and 50 is assigned to it.
+	*/
+	var safe bool
 	safe, speed := true, 50
 	fmt.Println(safe, speed)
 
