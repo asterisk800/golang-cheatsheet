3226505a18cf2c685762f9162acdf8ce4b427b51
Gudina H <asterisk800@users.noreply.github.com>
2021-01-30 13:57:03 -0800

    Added group-variables

---
+++ group-variables/group-variables.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+// Use group declaration for a better readability
+var (
+	name  string
+	speed float64
+	ratio float64
+)
+
+var speed1, ratio2 float64
+
+func main() {
+
+	fmt.Println(name, speed, ratio, speed1, ratio2)
+}
